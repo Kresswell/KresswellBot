25a7c4c2164f1f94b3ee5ee19fe66d1e9caf0d16
Kresswell <130984721+Kresswell@users.noreply.github.com>
2023-10-08 23:57:55 +0300

    Update _antibadword.js

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -1,7 +1,7 @@
 let { GroupSettingChange } = require('@adiwajshing/baileys')
 let handler = m => m
 
-let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|mbwa|titi(t|d)|pe?ler|matako|malaya|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|kuma|dajj?al|janc(u|o)k|ngwati|puki ?(mak)?|pussy|fala|sex|col(i|mek?)|nudes|porn|nigga|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
+let badwordRegex = /anj(k|g)|ajn?(g|k)|a?njin(g|k)|bajingan|b(a?n)?gsa?t|ko?nto?l|me?me?(k|q)|pe?pe?(k|q)|mbwa|titi(t|d)|pe?ler|matako|malaya|ngewe|go?blo?k|to?lo?l|idiot|(k|ng)e?nto?(t|d)|jembut|kuma|dajj?al|janc(u|o)k|ngwati|puki ?(mak)?|pussy|fala|sex|col(i|mek?)|nudes|porn|kinuthia|fuck|dick|bitch|tits|bastard|asshole/i // tambahin sendiri
 
 handler.before = function (m, { isOwner, isBotAdmin }) {
     if (m.isBaileys && m.fromMe) return !0
@@ -18,7 +18,7 @@ If the warning reaches 5 you will be banned
 type *#on antibadword* to turn on antibadword
 type *#sorry* to reduce the warning
 
-"Never use a big word when a little filthy one will do" (Dreaded-Botto).`, watermark, 'Turn on Antibadword', ',1 antibadword',  'sorry', m)
+"Master told me to make sure you have manners" (KRESSWELL).`, watermark, 'Turn on Antibadword', ',1 antibadword',  'sorry', m)
         if (user.warning >= 5) {
             user.banned = true
             if (m.isGroup) {
